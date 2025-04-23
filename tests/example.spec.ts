c5210836466c2c7f5cb3c58dc8635d9338a0dcb1
nahian007 <nahian007@gmail.com>
2025-04-23 19:48:08 +0600

    First Version

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
+import { console } from 'inspector';
 
-test('has title', async ({ page }) => {
+test('Pull Text', async ({ page }) => {
   await page.goto('https://www.amazon.in/');
 
   // Search With Product Name
@@ -9,5 +10,20 @@ test('has title', async ({ page }) => {
 
   // Click on First Result
   await page.locator('.autocomplete-results-container #sac-suggestion-row-1').click()
+
+  const searchResults = await page.locator('[data-component-type="s-search-result"]')
+
+  // Loop for getting first three data
+  for(let i = 0; i < 3; i++){
+
+  const productTitle = await searchResults.nth(i).locator('[data-cy="title-recipe"]').getByRole('heading').first().textContent()
+  const productDescription = await searchResults.nth(i).locator('[data-cy="title-recipe"]').getByRole('heading').nth(1).textContent()
+  const productPrice = await searchResults.nth(i).locator('[data-cy="price-recipe"] [aria-describedby="price-link"] .a-price-whole').textContent()
+
+  console.log(productPrice)
+  console.log(productTitle)
+  console.log(productDescription)
+
+  }
 
 });
