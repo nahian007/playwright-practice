31a74a7ad4771e7c9c546eec6e399edda9e479bc
nahian007 <nahian007@gmail.com>
2025-04-23 15:26:16 +0600

    Navigating till search result

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,4 +2,12 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://www.amazon.in/');
+
+  // Search With Product Name
+  await page.locator('#nav-search').getByPlaceholder('Search Amazon.in').click()
+  await page.locator('#nav-search').getByPlaceholder('Search Amazon.in').fill('kurti for woman')
+
+  // Click on First Result
+  await page.locator('.autocomplete-results-container #sac-suggestion-row-1').click()
+
 });
